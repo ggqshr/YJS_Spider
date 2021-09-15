910f59e74468efd880d81dcbf1e3b3ba37fa2aea
ggqshr <36805478+ggqshr@users.noreply.github.com>
2021-09-15 09:23:50 +0800

    Update settings.py

--- YJS/settings.py
+++ YJS/settings.py
@@ -116,12 +116,12 @@ logging.basicConfig(
         RotatingFileHandler(filename='logs/YJS{}.log'.format(datetime.now().strftime("%Y.%m.%d")), encoding='utf-8',backupCount=3)],
     format='%(asctime)s %(name)s %(levelname)s %(message)s'
 )
-REDIS_HOST = "0.0.0.0"
+REDIS_HOST = "redis"
 REDIS_PORT = 6379
 
 MODE = "YAO"  # or YAO
 
-MONGODB_HOST = "0.0.0.0"
+MONGODB_HOST = "gateway"
 MONGODB_PORT = 10021
 
 apiUrl = "http://api.xdaili.cn/xdaili-api//greatRecharge/getGreatIp?spiderId=2eeedc14918546f087abcddafd5ee37d&orderno=YZ20196121637TQppQw&returnType=2&count=3"
